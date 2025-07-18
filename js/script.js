567f31ab439c0402e59021f095ee42ba4796f676
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-19 02:07:28 +0300

    popular tv shows

--- js/script.js
+++ js/script.js
@@ -36,6 +36,42 @@ async function displayPopularMovies() {
 	});
 }
 
+// popular Tv shows
+async function displayPopularShows() {
+	const { results } = await fetchData('tv/popular');
+
+	results.forEach((show) => {
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+							show.poster_path
+								? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+								: `								<img
+									src="images/no-image.jpg"
+									class="card-img-top"
+									alt="${show.name}"
+								/>`
+						}
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${
+								show.first_air_date
+							} </small>
+            </p>
+          </div>`;
+
+		document.querySelector('#popular-shows').appendChild(div);
+	});
+}
+
 // Fetch data from TMDB API
 
 async function fetchData(endpoint) {
@@ -84,7 +120,7 @@ function init() {
 			displayPopularMovies();
 			break;
 		case '/shows.html':
-			console.log('Shows');
+			displayPopularShows();
 			break;
 		case '/movie-details.html':
 			console.log('Movie Details');
